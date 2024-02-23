0f7f9e65904a8974058514f5dca6b4839269073b
Emrah <emrahketzo@gmail.com>
2024-02-23 14:07:04 +0100

    component personal - style terminé

--- src/components/Personal/Personal.jsx
+++ src/components/Personal/Personal.jsx
@@ -3,23 +3,27 @@ import './Personal.css';
 export default function Personal(props) {
     return(
         <div className='divPersonal'>
-            <p>Personal Info</p>
-            <p>Please provide your name, email address, and phone number.</p>
-            <div>
+            <div className='personalTitre'>
+                <p className='titreInfo'>Personal info</p>
+                <p className='paraTitre'>Please provide your name, email address, and phone number.</p>
+            </div>
+            <div className='personalForm'>
                 <div className='divInput'>
                     <label htmlFor="">Your name is :</label>
-                    <input type="text" placeholder='Name'/>
+                    <input type="text" placeholder='Name' className='inputMain'/>
                 </div>
                 <div className='divInput'>
-                    <label htmlFor="">Your name is :</label>
-                    <input type="text" placeholder='Name'/>
+                    <label htmlFor="">Your email is :</label>
+                    <input type="text" placeholder='@email.com' className='inputMain'/>
                 </div>
                 <div className='divInput'>
-                    <label htmlFor="">Your name is :</label>
-                    <input type="text" placeholder='Name'/>
+                    <label htmlFor="">Your phone number is :</label>
+                    <input type="text" placeholder='+32' className='inputMain'/>
                 </div>
             </div>
-            <div>Next Step</div>
+            <div className='personalBtn'>
+                <div className='btnNext'>Next Step</div>
+            </div>
         </div>
     )
 }
